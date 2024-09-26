2e642278f4c8e7d1cba439dc001a73f02f802ebc
escueladonfern <ueerdonfernandodepenalver@gmail.com>
2024-09-26 18:08:21 -0400

    Add files via upload

---
+++ src/Pdfs/Documentos.js
@@ -0,0 +1,56 @@
+// Somos
+import AcuerdosConvivencia from './AcuerdosConvivencia.pdf';
+
+// Docs
+import ListaCotejo from './ListaCotejo.pdf';
+import EscalaEstimacion from './EscalaEstimacion.pdf';
+import ResumenEstadistico from './ResumenEstadistico.pdf';
+import EstadisticaMensual from './EstadisticaMensual.pdf';
+import Horarios from './Horarios.pdf';
+import PA from './PA.pdf';
+import AmbiApre from './AmbientesAprendizaje.pdf';
+import HorarioMusica from './HorarioMusica.pdf';
+
+// CLIF
+import CuadOrieConsPepi from './CuadernilloDeOrientacionesParaConstruccionDelPepi.pdf';
+import OrieDesaCurr from './OrientacionesParaElDesarrolloCurricular.pdf';
+import MateVidaProfRosaBece from './MatematicaParaLaVidaProfRosaBecerra.pdf';
+import Cuaderno1 from './Cuaderno1.pdf';
+import Cuaderno2 from './Cuaderno2.pdf';
+import Cuaderno3 from './Cuaderno3.pdf';
+import GuiaDocenPlanEstrTempFortApreEducPrim from './GuiaDocentePlanEstrategiocoTemporalParaElFortalecimientoDeLosAprendizajesEnLaEducacionPrimaria.pdf';
+import LaboCaliEducProfLeniRome from './LaboratorioDeLaCalidadEducativaProfLeninRomero.pdf';
+import GestCurrPlanEstrTemp from './LaGestionCurricularElPlanEstrategicoTemporal.pdf';
+import Liderazgo from './Liderazgo.pdf';
+import MaterialesLengua from './MaterialesLengua.pdf';
+import PlanNaciSemiCien from './PlanNacionalSemilleroCientífico.pdf';
+
+// Pedagogia
+import Calendario from './CalendarioEscolar2024-2025-1.pdf';
+import Planificacion from './Planificacion.pdf';
+
+export default {
+	"Somos0" : AcuerdosConvivencia,
+	"Docs0" : ListaCotejo,
+	"Docs1" : EscalaEstimacion,
+	"Docs2" : ResumenEstadistico,
+	"Docs3" : EstadisticaMensual,
+	"Docs4" : Horarios,
+	"Docs5" : PA,
+	"Docs6" : AmbiApre,
+	"Docs7" : HorarioMusica,
+	"CLIF0" : CuadOrieConsPepi,
+	"CLIF1" : OrieDesaCurr,
+	"CLIF2" : MateVidaProfRosaBece,
+	"CLIF3" : Cuaderno1,
+	"CLIF4" : Cuaderno2,
+	"CLIF5" : Cuaderno3,
+	"CLIF6" : GuiaDocenPlanEstrTempFortApreEducPrim,
+	"CLIF7" : LaboCaliEducProfLeniRome,
+	"CLIF8" : GestCurrPlanEstrTemp,
+	"CLIF9" : Liderazgo,
+	"CLIF10" : MaterialesLengua,
+	"CLIF11" : PlanNaciSemiCien,
+	"Peda0" : Calendario,
+	"Peda1" : Planificacion,
+};
